814f5db8bcbba9f5713d0f5413a5dcb57519e327
Karoline Correa <ckaroline226@gmail.com>
2023-01-03 21:56:28 -0300

    Desenvolvendo o JS do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var texto = document.getElementById('textoinserido').value
+
+
+
+
+var decodificar = textoinserido.replace(
+    ("e", "enter"),
+    ("i", "imes"),
+    ("a", "ai"),
+    ("o", "ober"),
+    ("u", "ufat")
+)
