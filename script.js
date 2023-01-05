5aba673ff3d0c1c430d0c4b0ead56edb28e63561
Karoline Correa <ckaroline226@gmail.com>
2023-01-04 22:06:50 -0300

    Estudando a função Onclick

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 
 var texto = document.getElementById('textoinserido').value
 
-
-
-
-var decodificar = textoinserido.replace(
+var substituir = textoinserido.replace(
     ("e", "enter"),
     ("i", "imes"),
     ("a", "ai"),
     ("o", "ober"),
     ("u", "ufat")
 )
+
+// var reverter = (
+//     ("enter", "e"),
+//     ("imes", "i"),
+//     ("ai", "a"),
+//     ("ober", "o"),
+//     ("ufat", "u")
+// )
+
+
+
+function criptografar(){
+    var funcao = substituir()
+
+}
+
+ 
+var btn = document.getElementById("criptografar")
+btn.addEventListener("click", criptografar)
+
+
